e89e117cbd71f16071bf60ea995eb375bf29968b
dhubleur <dhubleur@student.42.fr>
2024-01-23 14:19:05 +0100

    my own pbkdf1

--- srcs/algorithms/pbkdf1.c
+++ srcs/algorithms/pbkdf1.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 14:49:40 by dhubleur          #+#    #+#             */
-/*   Updated: 2024/01/06 15:32:36 by dhubleur         ###   ########.fr       */
+/*   Updated: 2024/01/23 14:15:44 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,25 @@
 
 bool	pbkdf1(char *password, uint8_t salt[8], uint8_t key[8])
 {
-	char *concat = malloc(sizeof(char) * (ft_strlen(password) + 8 + 1));
-	if (!concat)
-		return false;
-	ft_strcpy(concat, password);
-	ft_memcpy(concat + ft_strlen(password), salt, 8);
+	_sha256_context_t context;
 
-	uint8_t sha256_res[32];
-	sha256_string(concat, sha256_res);
+	_sha256_init(&context);
+	_sha256_fill_and_run_if_complete(&context, (uint8_t *)password, ft_strlen(password));
+	_sha256_fill_and_run_if_complete(&context, salt, 8);
+	_sha256_padd_and_finalize(&context);
 
-	char input[33];
-	for (int i = 0; i < PBKDF1_ITERATIONS-1; i++)
+	uint8_t store[32];
+	ft_memcpy(store, context.digest, 32);
+
+	for (int i = 1; i < PBKDF1_ITERATIONS; i++)
 	{
-		ft_memcpy(input, sha256_res, 32);
-		input[32] = '\0';
-		sha256_string(input, sha256_res);
+		_sha256_init(&context);
+		_sha256_fill_and_run_if_complete(&context, store, 32);
+		_sha256_padd_and_finalize(&context);
+		ft_memcpy(store, context.digest, 32);
 	}
-	ft_memcpy(key, sha256_res, 8);
+
+	ft_memcpy(key, store, 8);
 
 	return true;
 }
